73476a4f0904cd17d5e4d12f42b98230a26ae1e6
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2014-02-25 01:26:59 +0400

    Initial commit

---
+++ AppDelegate.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace GitHubClient
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		private UIWindow window;
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			window.MakeKeyAndVisible();
+
+			return true;
+		}
+	}
+}
